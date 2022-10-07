56dd16a412710b8a5bb41cf16508d1dd26758eb0
Doguscan Yaginli <doguscanyaginli@gmail.com>
2022-10-07 15:09:04 -0500

    changed greeting

--- index.js
+++ index.js
@@ -15,9 +15,7 @@ const app = express();
 
 app.get('/', (req, res) => {
 
-    res.send({ 
-        hi: 'there', 
-    });
+    res.send({hi: 'buddy'});
 
 });
 
@@ -29,7 +27,30 @@ app.get('*', (req, res)=>{
 
 });
 
+//HEROKU choses the port or 5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
 
+//git init
+//add .
+
+
+/**
+ * 
+ * 
+ * heroku create
+ » Warning: heroku update available from 7.53.0 to 7.64.0.
+Creating app... done, ⬢ desolate-taiga-80269
+https://desolate-taiga-80269.herokuapp.com/ | https://git.heroku.com/desolate-taiga-80269.git
+git remote add heroku https://git.heroku.com/desolate-taiga-80269.git
+error: remote heroku already exists.
+git push heroku master
+ */
+
+
+/**RE-DEPLOY FILE
+ * 
+ * 
+ * 
+*/
